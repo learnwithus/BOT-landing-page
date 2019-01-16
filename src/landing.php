a820f62e4af8dbd4fc97ff50869b08ab34c383be
Josh Stuible <shrewdlove@gmail.com>
2019-01-16 12:58:41 -0800

    Formatted page

--- src/landing.php
+++ src/landing.php
@@ -19,17 +19,21 @@
     <main>
         <div class="container">
             <div class="row">
-            <div class="col-sm-6"></div>
-                <div class="col-sm-6">
+                <div class="col-xs-12">
+                    <h1>VCH</h1>
+                    <h2>BOT</h2>
+                </div>
+                <div class="col-xs-6"></div>
+                <div class="col-xs-6">
                 <?php
                     if (!is_user_logged_in()) { // Display WordPress login form:
                         $args = array(
                             'redirect' => admin_url(),
                             'form_id' => 'loginform-custom',
-                            'label_username' => __('Username custom text'),
-                            'label_password' => __('Password custom text'),
-                            'label_remember' => __('Remember Me custom text'),
-                            'label_log_in' => __('Log In custom text'),
+                            'label_username' => __('Username'),
+                            'label_password' => __('Password'),
+                            'label_remember' => __('Remember Me'),
+                            'label_log_in' => __('Log In'),
                             'remember' => true,
                         );
                         wp_login_form($args);
