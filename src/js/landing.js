e0a55e883609b9bf7e7d886b13d708fc419418be
Josh Stuible <shrewdlove@gmail.com>
2019-02-22 13:45:22 -0800

    Added placeholder for Password in javscript (hacky but it works)
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
-    $('input#user_login').attr('placeholder', 'username');
+    $('input#user_login').attr('placeholder', 'Username');
+    $('input#user_pass').attr('placeholder', 'Password');
 });
